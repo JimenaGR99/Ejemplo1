1fd4e1d14d1be95785d96cc11e610169a90e4a28
JimenaGR99 <jjgarzonr@udistrital.edu.co>
2023-04-29 07:49:25 -0500

    primer codigo :100:

---
+++ Abril29.py
@@ -0,0 +1,7 @@
+#import backend
+from backend.Areas import *
+
+print('Prueba')
+
+AreaCirculo(100)
+AreaCuadrado(100)
---
+++ backend/Areas.py
@@ -0,0 +1,11 @@
+'''
+En este espacio se calculan diferentes áreas
+'''
+
+import math
+
+def AreaCirculo(radio):
+    print(math.pi * radio**2)
+
+def AreaCuadrado(lado):
+    print(lado**2)
